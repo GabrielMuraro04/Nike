b15f913c4d49016f27d300c620c843d13a59dc9f
Gabriel Muraro <gabrielmurarodev@hotmail.com>
2025-10-03 17:53:16 -0300

    feat: adiciona botões para trocar cor de fundo e imagem do tênis

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let body = document.querySelector("body")
+let tenis = document.querySelector(".img-tenis")
+
+function mudarVisual(cor, imagem) {
+    body.style.background = cor
+    tenis.src = imagem
+}
